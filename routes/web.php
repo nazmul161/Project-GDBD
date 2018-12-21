fca70e087f85027930844c8f6909a698d1e9693a
MohammadSohail <sohailnsu15@gmail.com>
2018-12-21 22:06:22 +0600

    updated

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 Route::post('save_hotel_info','HomeController@save_hotel_info_func');
 
 Route::get('new_hotel', 'HomeController@new_hotel_func');
+
+Route::get('save_subscription/{user_id}/{pkg_id}', 'HomeController@save_subscription_func');
+
+Auth::routes();
