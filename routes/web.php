243649bbd5c85ccfe6cdc898d7fe34070c64e2cb
MdTamimAbdullah <mdtamimabdullah@gmail.com>
2018-12-22 20:43:21 +0600

    updated route

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('new_package', 'HomeController@new_package_func');
 
 Route::post('save_package_info','HomeController@save_package_info_func');
+
+Route::get('new_apartment', 'HomeController@new_apartment_func');
